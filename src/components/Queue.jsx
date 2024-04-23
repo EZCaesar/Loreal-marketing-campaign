111a93ec0029186eacd04aa80119ce4bfa5a7441
tntchn <15895303+tntchn@users.noreply.github.com>
2024-04-24 05:42:09 +0800

    Implement queue page layout

--- src/components/Queue.jsx
+++ src/components/Queue.jsx
@@ -2,6 +2,18 @@ import BackToSystemButton from "./BackToSystemButton";
 import { useRef } from 'react';
 import { createClient } from '@supabase/supabase-js'
 
+import frame from '../assets/main/frame-h380.png';
+import picUp from '../assets/main/pic-up.png';
+
+import title from '../assets/queue/queue-title.png';
+import text1 from '../assets/queue/queue-text1.png';
+import text2 from '../assets/queue/queue-text2.png';
+import text3 from '../assets/queue/queue-text3.png';
+import pic1 from '../assets/queue/queue-pic1.png';
+import pic2 from '../assets/queue/queue-pic2.png';
+import pic3 from '../assets/queue/queue-pic3.png';
+import btn from '../assets/queue/queue-btn.png';
+
 export default function Queue({handleBackToSystemClick}) {
   const nameRef = useRef();
   const phoneRef = useRef();
@@ -29,22 +41,59 @@ export default function Queue({handleBackToSystemClick}) {
   };
 
   return (
-    <div>
-      <div>
-        <h2>排隊候位系統</h2>
-        <label>
-          <p>請問如何稱呼您?</p>
-          <p>(ex: 林小姐)</p>
-          <input type="text" ref={nameRef} />
-        </label>
-        <label>
-          請問您的電話號碼?
-          <input type="tel" ref={phoneRef} />
-        </label>
-        <button onClick={joinQueue}>送出資料</button>
-        <p>契爾氏將記訊息通知您到櫃時間</p>
+    <div className='queue'>
+      <div className='frame-h380'>
+        <img src={frame} alt='' />
+        <div className='frame-h380-bg'></div>
       </div>
-      <BackToSystemButton handleBackToSystemClick={handleBackToSystemClick}></BackToSystemButton>
+
+      <div className='pic-up-h380'>
+        <img src={picUp} alt='' />
+      </div>
+
+      <h1 className='queue-title'>
+        <img src={title} alt='請輸入資料以領取排隊號碼' />
+      </h1>
+
+      <form className='queue-form'>
+        <label className='queue-label queue-input-1'>
+          <div>
+            <img src={text1} alt='請問如何稱呼您？' />
+          </div>
+          <input type="text" ref={nameRef} className='queue-input' />
+        </label>
+
+        <label className='queue-label queue-input-2'>
+          <div>
+            <img src={text2} alt='請問您的手機號碼？' />
+          </div>
+          <input type="tel" ref={phoneRef} className='queue-input' />
+        </label>
+
+        <button onClick={joinQueue} className='queue-btn'>
+          <img src={btn} alt='送出資料' />
+        </button>
+      </form>
+
+      <div className='queue-text3'>
+        <img src={text3} alt='' />
+      </div>
+
+      <div className='queue-pic1'>
+        <img src={pic1} alt='' />
+      </div>
+
+      <div className='queue-pic2'>
+        <img src={pic2} alt='' />
+      </div>
+
+      <div className='queue-pic3'>
+        <img src={pic3} alt='' />
+      </div>
+
+      <footer className='queue-back-btn'>
+        <BackToSystemButton handleBackToSystemClick={handleBackToSystemClick} />
+      </footer>
     </div>
   );
 }
