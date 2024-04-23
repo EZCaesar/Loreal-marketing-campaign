a5074312e438a95c930e025833ab3132f7299c78
EZCaesar <62068073+EZCaesar@users.noreply.github.com>
2024-04-24 00:31:48 +0800

    Add .env to .gitignore and update package.json dependencies

--- src/components/Queue.jsx
+++ src/components/Queue.jsx
@@ -1,6 +1,32 @@
 import BackToSystemButton from "./BackToSystemButton";
+import { useRef } from 'react';
+import { createClient } from '@supabase/supabase-js'
 
 export default function Queue({handleBackToSystemClick}) {
+  const nameRef = useRef();
+  const phoneRef = useRef();
+  console.log(process.env.REACT_APP_SUPABASE_URL);
+  const supabase = createClient(process.env.REACT_APP_SUPABASE_URL, process.env.REACT_APP_SUPABASE_KEY);
+
+  // Function to add a user to the queue
+  const joinQueue = async () => {
+    const phone = phoneRef.current.value;
+    const displayedName = nameRef.current.value;
+    try{
+      if (!phone || !displayedName) {
+        throw new Error('請填寫完整資料');
+      }
+      await supabase
+      .from('user')
+      .insert([{ userId: Date.now(), phone, displayedName, status: 'waiting' }]);
+ 
+      alert('已成功加入排隊');
+    } catch (error) {
+      alert(error.message);
+    }
+  
+  };
+
   return (
     <div>
       <div>
@@ -8,13 +34,13 @@ export default function Queue({handleBackToSystemClick}) {
         <label>
           <p>請問如何稱呼您?</p>
           <p>(ex: 林小姐)</p>
-          <input type="text" />
+          <input type="text" ref={nameRef} />
         </label>
         <label>
           請問您的電話號碼?
-          <input type="tel" />
+          <input type="tel" ref={phoneRef} />
         </label>
-        <button>送出資料</button>
+        <button onClick={joinQueue}>送出資料</button>
         <p>契爾氏將記訊息通知您到櫃時間</p>
       </div>
       <BackToSystemButton handleBackToSystemClick={handleBackToSystemClick}></BackToSystemButton>
