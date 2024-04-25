358f6a2cf37a2e39fe0404f7fa65b2ecfb6f7252
tntchn <15895303+tntchn@users.noreply.github.com>
2024-04-25 11:13:52 +0800

    Implement new product details design

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,12 +1,12 @@
-import back from '../assets/main/back.png';
+import back from '../assets/product/product-back.png';
 
 export default function ProductDetails({ product, renderProductDetails, setProduct }) {
   return (
     <div>
       {renderProductDetails(product)}
       <footer className='product-footer'>
-        <button onClick={() => setProduct(null)} className='back-btn'>
-          <img src={back} alt='返回上一頁' />
+        <button onClick={() => setProduct(null)} className='product-back-btn'>
+          <img src={back} alt='返回上一頁，回答其他題目' />
         </button>
       </footer>
     </div>
