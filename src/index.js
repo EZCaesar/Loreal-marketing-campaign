34d64099c81b5a984010db1f09ce11b7c4d5a4b4
EZCaesar <62068073+EZCaesar@users.noreply.github.com>
2024-04-27 15:02:48 +0800

    Add @vercel/analytics and @vercel/speed-insights packages for analytics and speed insights tracking

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Admin from './Admin';
 import Clients from './Clients';
 import Home from './Home';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,6 +16,8 @@ root.render(
         <Route path="/:branch/admin" element={<Admin />} />
         <Route path="/:branch/clients" element={<Clients />} />
       </Routes>
+      <Analytics />
+      <SpeedInsights />
     </Router>
   </React.StrictMode>
 );
